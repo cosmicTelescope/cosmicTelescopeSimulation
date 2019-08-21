5b0a60fb6d7a1d4fcf92dffa77b4be6e671f2a15
phyxch <phyxch@gmail.com>
2019-08-21 11:10:25 -0400

    Add event type messenger options

--- include/CTPrimaryGeneratorAction.hh
+++ include/CTPrimaryGeneratorAction.hh
@@ -1,6 +1,8 @@
 // June 4, 2019: hexc, James and Nadia
-// First implementation of cosmic ray telescope simulation based on B4a example.
+//     First implementation of cosmic ray telescope simulation based on B4a example.
 //     The telescope consists of three layers of plastic scintillator
+// August 20, 2019: hexc, James, and Nadia
+//     Add generator private members
 // 
 /// \
 /// \file B4PrimaryGeneratorAction.hh
@@ -10,6 +12,8 @@
 #define CTPrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
+#include "CTPrimaryGeneratorMessenger.hh"
+
 #include "globals.hh"
 
 class G4ParticleGun;
@@ -25,16 +29,19 @@ class G4Event;
 class CTPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
 public:
-  CTPrimaryGeneratorAction();    
-  virtual ~CTPrimaryGeneratorAction();
-
-  virtual void GeneratePrimaries(G4Event* event);
-  
-  // set methods
-  void SetRandomFlag(G4bool value);
-
+    CTPrimaryGeneratorAction();    
+    virtual ~CTPrimaryGeneratorAction();
+    
+    virtual void GeneratePrimaries(G4Event* event);
+    
+    // set methods
+    void SetRandomFlag(G4bool value);
+    void SetEventType(G4int eventType) { ev_type = eventType; };
+    
 private:
-  G4ParticleGun*  fParticleGun; // G4 particle gun
+    G4ParticleGun*  fParticleGun; // G4 particle gun
+    CTPrimaryGeneratorMessenger* gunMessenger;
+    G4int ev_type, evtID;
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
--- src/CTPrimaryGeneratorAction.cc
+++ src/CTPrimaryGeneratorAction.cc
@@ -1,8 +1,10 @@
 // June 4, 2019: hexc, James and Nadia
-// First implementation of cosmic ray telescope simulation based on B4a example.
+//     First implementation of cosmic ray telescope simulation based on B4a example.
 //     The telescope consists of three layers of plastic scintillator
 // June 5, 2019
-// Try out particle gun properties
+//     Try out particle gun properties
+// August 20, 2019: hexc, James and Nadia
+//     Update the primary generator to simulation "real" cosmic ray flux
 /// 
 /// \file B4PrimaryGeneratorAction.cc
 /// \brief Implementation of the B4PrimaryGeneratorAction class
@@ -26,16 +28,23 @@ CTPrimaryGeneratorAction::CTPrimaryGeneratorAction()
  : G4VUserPrimaryGeneratorAction(),
    fParticleGun(nullptr)
 {
-  G4int nofParticles = 1;
-  fParticleGun = new G4ParticleGun(nofParticles);
+    // set event type
+    ev_type = 1;
 
-  // default particle kinematic
-  //
-  auto particleDefinition 
-    = G4ParticleTable::GetParticleTable()->FindParticle("e-");
-  fParticleGun->SetParticleDefinition(particleDefinition);
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,1.,0.));
-  fParticleGun->SetParticleEnergy(50.*MeV);
+    G4int nofParticles = 1;
+    fParticleGun = new G4ParticleGun(nofParticles);
+    
+  
+    //create a messenger for this class
+    gunMessenger = new CTPrimaryGeneratorMessenger(this);
+
+    // default particle kinematic
+    //
+    auto particleDefinition 
+	= G4ParticleTable::GetParticleTable()->FindParticle("mu-");
+    fParticleGun->SetParticleDefinition(particleDefinition);
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,1.,0.));
+    fParticleGun->SetParticleEnergy(50.*MeV);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -49,43 +58,112 @@ CTPrimaryGeneratorAction::~CTPrimaryGeneratorAction()
 
 void CTPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-  // This function is called at the begining of event
+    // This function is called at the begining of event
+    G4ParticleDefinition* particle;
 
-  // In order to avoid dependence of PrimaryGeneratorAction
-  // on DetectorConstruction class we get world volume 
-  // from G4LogicalVolumeStore
-  //
-  G4double worldZHalfLength = 0.;
-  auto worldLV = G4LogicalVolumeStore::GetInstance()->GetVolume("world_volume");
+    G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+    //particleTable ->DumpTable("ALL");
+    
+    // In order to avoid dependence of PrimaryGeneratorAction
+    // on DetectorConstruction class we get world volume 
+    // from G4LogicalVolumeStore
+    //
+    G4double worldZHalfLength = 0.;
+    auto worldLV = G4LogicalVolumeStore::GetInstance()->GetVolume("world_volume");
+    
+    // Check that the world volume has box shape
+    G4Box* worldBox = nullptr;
+    if (  worldLV ) {
+	G4cout << "We found the world! " << G4endl;
+	worldBox = dynamic_cast<G4Box*>(worldLV->GetSolid());
+    }
+    
+    if ( worldBox ) {
+	worldZHalfLength = worldBox->GetZHalfLength();  
+	G4cout << " worldZHalfLength = " << worldZHalfLength << G4endl;
+    }
+    else  {
+	G4ExceptionDescription msg;
+	msg << "World volume of box shape not found." << G4endl;
+	msg << "Perhaps you have changed geometry." << G4endl;
+	msg << "The gun will be place in the center.";
+	G4Exception("CTPrimaryGeneratorAction::GeneratePrimaries()",
+		    "MyCode0002", JustWarning, msg);
+    } 
+    
+    // Set up cosmic ray events
+    if (ev_type == 0) {
+	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,-1.,0.));
+	fParticleGun->SetParticleEnergy(1.*GeV);
+	fParticleGun->SetParticlePosition(G4ThreeVector( (G4UniformRand()-0.5)*30.0*cm, 
+							 25.0*cm,
+							 (G4UniformRand()-0.5)*30.0*cm) );
+	G4double sigmaAngle = 140.*deg;
+		
+	G4double theta = (G4UniformRand()-0.5)*sigmaAngle;
+	G4double phi = G4UniformRand()*360.*deg;
+	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(std::sin(phi)*std::sin(theta), -std::cos(theta), std::cos(phi)*std::sin(theta)));
+	
+	fParticleGun->GeneratePrimaryVertex(anEvent);
 
-  // Check that the world volume has box shape
-  G4Box* worldBox = nullptr;
-  if (  worldLV ) {
-      G4cout << "We found the world! " << G4endl;
-    worldBox = dynamic_cast<G4Box*>(worldLV->GetSolid());
-  }
-
-  if ( worldBox ) {
-    worldZHalfLength = worldBox->GetZHalfLength();  
-    G4cout << " worldZHalfLength = " << worldZHalfLength << G4endl;
-  }
-  else  {
-    G4ExceptionDescription msg;
-    msg << "World volume of box shape not found." << G4endl;
-    msg << "Perhaps you have changed geometry." << G4endl;
-    msg << "The gun will be place in the center.";
-    G4Exception("CTPrimaryGeneratorAction::GeneratePrimaries()",
-      "MyCode0002", JustWarning, msg);
-  } 
-  
-  // Set gun position
-  //  fParticleGun
-  //    ->SetParticlePosition(G4ThreeVector(0., 0., -worldZHalfLength));
-  //fParticleGun
-  //  ->SetParticlePosition(G4ThreeVector(0., 0., -worldZHalfLength));
-
-  fParticleGun->GeneratePrimaryVertex(anEvent);
+    }  else if (ev_type == 1) {                  
+	particle = particleTable->FindParticle("mu-");
+	fParticleGun->SetParticleDefinition(particle);
+	
+	//fParticleGun->SetParticleTime(0.0*ns);
+	//fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
+	
+	G4double momentum = 300.*MeV;
+	G4double sigmaMomentum = 10000.*MeV;
+	
+	G4double mass = particle->GetPDGMass();
+	
+	G4double sigmaAngle = 140.*deg;
+	
+	G4int npart = 10;
+	for (G4int i=1; i<=npart; i++) {
+	    G4double pp = momentum + G4UniformRand()*sigmaMomentum;  // 0 GeV < pp << 10.3 GeV    
+	    G4double Ekin = std::sqrt(pp*pp+mass*mass) - mass;
+	    fParticleGun->SetParticleEnergy(Ekin);
+	    fParticleGun->SetParticlePosition(G4ThreeVector( (G4UniformRand()-0.5)*30.0*cm, 
+							     25.0*cm,
+							     (G4UniformRand()-0.5)*30.0*cm) );
+	        
+	    G4double theta = (G4UniformRand()-0.5)*sigmaAngle;
+	    G4double phi = G4UniformRand()*360.*deg;
+	    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(std::sin(phi)*std::sin(theta), -std::cos(theta), std::cos(phi)*std::sin(theta)));
+	    
+	    fParticleGun->GeneratePrimaryVertex(anEvent);
+	}
+    } else if (ev_type == 2) 
+	{
+	    particle = particleTable->FindParticle("neutron");
+	    fParticleGun->SetParticleDefinition(particle);
+	    
+	    G4double momentum = 1.*MeV;
+	    G4double sigmaMomentum = 100.*MeV;
+	    
+	    G4double mass = particle->GetPDGMass();
+	    
+	    G4double sigmaAngle = 140.*deg;
+	    
+	    G4int npart = 10;
+	    for (G4int i=1; i<=npart; i++) {
+		G4double pp = momentum + G4UniformRand()*sigmaMomentum;  // 0 GeV < pp << 10.3 GeV    
+		G4double Ekin = std::sqrt(pp*pp+mass*mass) - mass;
+		fParticleGun->SetParticleEnergy(Ekin);
+		fParticleGun->SetParticlePosition(G4ThreeVector( (G4UniformRand()-0.5)*30.0*cm, 
+								 25.0*cm,
+								 (G4UniformRand()-0.5)*30.0*cm) );
+	        
+		G4double theta = (G4UniformRand()-0.5)*sigmaAngle;
+		G4double phi = G4UniformRand()*360.*deg;
+		fParticleGun->SetParticleMomentumDirection(G4ThreeVector(std::sin(phi)*std::sin(theta), -std::cos(theta), std::cos(phi)*std::sin(theta)));
+		
+		fParticleGun->GeneratePrimaryVertex(anEvent);
+	    }
+	} else {
+	G4cout << "Dummy! you need to pick a proper event type. Choices are: 0, 1 or 2 for now." << G4endl;
+    }
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......	
